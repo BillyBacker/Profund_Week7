46882855ad4052708bc1db0d250c2d9088a1a6a2
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-10-10 13:03:46 +0700

    Edit graph sensitivity

--- Profund_Week7/Source.cpp
+++ Profund_Week7/Source.cpp
@@ -4,7 +4,7 @@
 #include<math.h>
 
 bool onGraph(float Param[3], float x, float y) {
-	if ((-Param[0]) * pow(x, 2) + Param[1] * x - Param[2] == y) {
+	if (abs((-Param[0]) * pow(x, 2) + Param[1] * x - Param[2] - y) <= 0.5) {
 		return true;
 	}
 	return false;
@@ -19,14 +19,14 @@ int main() {
 	}
 	for (float colum = -20; colum <= 20; colum+=1) {
 		for (float row = 20; row >= -20; row-=1) {
-			if (colum == 0 || row == 0) {
+			if ((colum == 0 || row == 0) && !onGraph(Param, row, colum)) {
 				printf("+");
 			}
-			else {
+			else if(!onGraph(Param, row, colum)) {
 				printf(" ");
 			}
-			if (onGraph(Param, row/2, colum/2)) {
-				printf("0");
+			if (onGraph(Param, row, colum)) {
+				printf("0 ");
 			}
 			else {
 				printf(" ");
